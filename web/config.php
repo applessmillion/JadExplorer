c0288c173ab5c7ba115f7b8bf3d6b72573e5ba5a
theapplessmillion <appless9009@gmail.com>
2018-02-05 10:34:40 -0500

    fixed config

--- web/config.php
+++ web/config.php
@@ -1,7 +1,6 @@
 <?php
-
+// For testing purposes, this user has FULL db permissions
 // Database server shared with Maralook.com
-M,
     $ip   = "107.180.46.160"; //MySQL Server IP (l27.0.0.1)
     $user = "shu-user"; //User for db
     $pw   = "appless"; //User's password
