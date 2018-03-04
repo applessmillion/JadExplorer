4e7bed81bfaaddb68ec704982d55634f538fc517
applessmillion <appless9009@gmail.com>
2018-03-04 11:23:19 -0600

    Added support for username search
    Username can now be used to search for assets. It is much like the asset name search in terms of functionality.

--- web/search-display.php
+++ web/search-display.php
@@ -1,12 +1,13 @@
 <?php
-#INCLUDE THE FOLLOWING TO MAKE THE REST WORK#
+//File is used to search for assets by name from search.php 
+
+//config.php file needed for MySQL connection
 include_once 'config.php';
 
+//Create instance for MySQL connection
 $con = new mysqli($ip,$user,$pw,$db);
-$names = $_GET["n"];
 ?>
 
-<!-- This will be the form to search for an asset tag number. Maybe even the username later? -->
 <html>
 <table align="center" width="780" style="background:#efefef">
 	<tr style="background:#ddd"><th colspan="3"><h2>Search For Assets</h2></th></tr>
@@ -14,26 +15,51 @@ $names = $_GET["n"];
 		<td style="height:20px"></td>
 	</tr
 	<?php
+	//If asset name is set, do this
+	if(ISSET($_GET['n'])){
+		$name = $_GET["n"];
+		#GET NAME FROM SEARCH TERMS#
+		$search_query = mysqli_query($con, "SELECT * FROM assets WHERE aname LIKE '%$name%' ORDER BY asset ASC LIMIT 25");
+		$search_nums = mysqli_num_rows($search_query);	
 
-    $name = $_GET["n"];
-    #GET NAME FROM SEARCH TERMS#
-    $search_query = mysqli_query($con, "SELECT * FROM assets WHERE aname LIKE '%$name%' ORDER BY asset ASC LIMIT 25");
-    $search_nums = mysqli_num_rows($search_query);	
-
-	echo '<tr><th colspan="3"><h2>Found '. $search_nums .' results for "'. $names . '"...</h2></th></tr>';
-    echo "<tr><td><b>Asset Name</b></td><td><b>Asset Tag</b></td><td><b>Asset Owner</b></td></th></tr>";
-    echo '<tr><th colspan="3"><hr style="border-color:#6D7ACE; width:85%;"></th></tr>';
+		echo '<tr><th colspan="3"><h2>Found '. $search_nums .' results for "'. $name . '"...</h2></th></tr>';
+		echo "<tr><td><b>Asset Name</b></td><td><b>Asset Tag</b></td><td><b>Asset Owner</b></td></th></tr>";
+		echo '<tr><th colspan="3"><hr style="border-color:#6D7ACE; width:85%;"></th></tr>';
 	
-	//List search variables in table rows.
-    while ($obj = mysqli_fetch_object($search_query)) {
+		//List search variables in table rows.
+		while ($obj = mysqli_fetch_object($search_query)) {
 		
-        echo "<tr><td><a class='reg' href='search.php?a=" . urlencode($obj->asset) . "'><b>" . $obj->aname ."</b></a></td>";
-		echo "<td>". $obj->asset ."</td>";
-		echo "<td>". $obj->owner ."</td></th></tr>";
+			echo "<tr><td><a class='reg' href='search.php?a=" . urlencode($obj->asset) . "'><b>" . $obj->aname ."</b></a></td>";
+			echo "<td>". $obj->asset ."</td>";
+			echo "<td>". $obj->owner ."</td></th></tr>";
 		
-    }   
-    echo '<tr><td style="height:20px;"></br><a href="javascript:history.go(-1)">Back to Search...</a></td></tr>';
+		}   
+		echo '<tr><td style="height:20px;"></br><a href="javascript:history.go(-1)">Back to Search...</a></td></tr>';
+	}
+	//If username is set, do this
+	elseif(ISSET($_GET['u'])){
+		$name = $_GET["u"];
+		#GET NAME FROM SEARCH TERMS#
+		$search_query = mysqli_query($con, "SELECT * FROM assets WHERE owner LIKE '%$name%' ORDER BY asset ASC LIMIT 25");
+		$search_nums = mysqli_num_rows($search_query);	
 
+		echo '<tr><th colspan="3"><h2>Found '. $search_nums .' results for '. $name . '</h2></th></tr>';
+		echo "<tr><td><b>Asset Name</b></td><td><b>Asset Tag</b></td><td><b>Asset Owner</b></td></th></tr>";
+		echo '<tr><th colspan="3"><hr style="border-color:#6D7ACE; width:85%;"></th></tr>';
+	
+		//List search variables in table rows.
+		while ($obj = mysqli_fetch_object($search_query)) {
+		
+			echo "<tr><td><a class='reg' href='search.php?a=" . urlencode($obj->asset) . "'><b>" . $obj->aname ."</b></a></td>";
+			echo "<td>". $obj->asset ."</td>";
+			echo "<td>". $obj->owner ."</td></th></tr>";
+		
+		}   
+		echo '<tr><td style="height:20px;"></br><a href="javascript:history.go(-1)">Back to Search...</a></td></tr>';
+	}
+	else{
+		echo "Error. Username and Asset Name not defined. Go back and try again?";
+	}
 	?>
 </table>
 </html>
--- web/search.php
+++ web/search.php
@@ -21,13 +21,33 @@ if(ISSET($_GET['a'])){
 <?php
 //Need to connect to mysql. Search DB like SELECT * FROM assets WHERE asset like %$_GET['a']%
 }
-//n = name. Used mostly for username searches. Maybe that would be more convenient...
+//Handle for asset name search
 elseif(ISSET($_GET['n'])){
 ?>
 <html>
 	<!-- Initalize Page -->
 	<head>
 		<?php echo '<title>SHU DoIT | Search ', $_GET['n'] ,'</title>'; ?>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	
+	<body>
+		<div id="main">
+			<?php echo file_get_contents('header.html') ?>
+			<br><br></br>
+			<?php include('search-display.php'); ?>
+		</div>
+	</body>
+</html>
+<?php
+}
+//Handle for username search
+elseif(ISSET($_GET['u'])){
+?>
+<html>
+	<!-- Initalize Page -->
+	<head>
+		<?php echo '<title>SHU DoIT | Search ', $_GET['u'] ,'</title>'; ?>
 		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 	
