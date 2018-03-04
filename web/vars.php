6c937254a41b358515438b3460931d795f35a41c
applessmillion <appless9009@gmail.com>
2018-03-04 11:04:47 -0600

    Added index description

--- web/vars.php
+++ web/vars.php
@@ -1,31 +1,16 @@
 <?php
-include_once('vars.php');
-//for future use
+//File for easy modification of text fields on various pages.
+
+$index_desc = "
+
+<center><h4>Welcome to BenWorks!</h4></center>
+BenWorks is an inventory management tool to keep pages on each asset added through our system.
+There are two types of assets that can be added - computer-related assets and audio-visual assets.
+Each of the creation pages offer unique options for each type of asset, and allow for it to be accessed
+through our search tools.
+Asset pages can be modified and updated at any time, in hopes to keep these records as up to date as possible.
+Each page also offers searching SpiceWorks for the asset, allowing for easy discovery of the devices through that tool.
+
+"
+
 ?>
-<html>
-	<head>
-		<title>SHU DoIT | Asset Catalog</title>
-		<link rel="stylesheet" type="text/css" href="style.css">
-	</head>
-	<body>
-		<?php
-		echo    file_get_contents('header.html') . "</br>";
-		?>
-		<div class="main">
-		<img src="img/corner.png" width="9"><img src="img/border.png" width="692" height="9" border="0"><img src="img/corner2.png" width="9">
-		<table align="center" width="710">
-			<tr>
-				<td style="background:#eee"></br>
-					<h2>Siena Heights University Asset Catalog</h2>
-					<p>
-					<?php $index_desc; ?>
-					</p>
-					</br>
-				</td>
-			</tr>
-			</p></th></tr>
-		</table>
-		<img src="img/corner3.png" width="9" ><img src="img/border.png" width="692" height="9" border="0"><img src="img/corner4.png" width="9">
-		</div>
-	</body>
-</html> 
