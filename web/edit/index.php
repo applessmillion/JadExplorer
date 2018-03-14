c39a20836d021d9a1c18adc62f20553ed337bbda
applessmillion <appless9009@gmail.com>
2018-03-14 00:28:21 -0400

    Added functions for editing

--- web/edit/index.php
+++ web/edit/index.php
@@ -1,46 +1,29 @@
 <?php //Page for editing asset pages.
 
-//Has the mySQL connection info
-include_once('config.php');
-
-//Connects to mySQL database
-$con = new mysqli($ip,$user,$pw,$db);
-
 //Pull and convert the GET variable into a php var.
-$asset = $_GET["a"];
+$asset = $_GET["edit"];
 
-//SQL statement to pull data from the ASSET table
-$assetsql = "SELECT * from shutest.assets WHERE asset='$asset';";
 
-//SQL statement to pull data from the LOCATION table
-$locsql = "SELECT * from shutest.locations WHERE asset='$asset';";
-
-if(ISSET($_GET['a'])){
+if(ISSET($_GET['edit'])){
 ?>
 <html>
 	<!-- Initalize Page -->
 	<head>
 		<?php echo '<title>SHU DoIT | Editing Asset# ', $_GET['a'] ,'</title>'; ?>
-		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="stylesheet" type="text/css" href="../style.css">
 	</head>
-	
 	<body>
+		<?php
+			include('header.html') . "</br>";
+		?>
 		<div id="main">
-			<?php echo file_get_contents('header.html') ?>
+			<?php include('../header.html') ?>
 			<br><br></br>
-			<?php 
-			//Copy and paste the file here, so php vars can be filled in.
-			include('asset-display-form.php'); ?>
+			<?php include('asset-edit.php') ?>
 		</div>
 	</body>
 </html>
 <?php
-//Need to connect to mysql. Search DB like SELECT * FROM assets WHERE asset like %$_GET['a']%
-}
-//n = name. Used mostly for username searches. Maybe that would be more convenient...
-elseif(ISSET($_GET['n'])){
-	echo 'LATER THAN SOON';
-	//Need to connect to mysql. Search DB like SELECT * FROM assets WHERE asset like %$_GET['a']%
 }
 else{
 ?>
