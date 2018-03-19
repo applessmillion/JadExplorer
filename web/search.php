09939504d9b9ed67dccbe947e0884093b55f021a
applessmillion <appless9009@gmail.com>
2018-03-19 03:45:07 -0400

    Improved Asset Search
    When an asset tag does not exist, or if a query returns more than one thing, it will now display a list

--- web/search.php
+++ web/search.php
@@ -1,25 +1,75 @@
 <?php
+include_once 'config.php';
+
+//Create instance for MySQL connection
+$con = new mysqli($ip,$user,$pw,$db);
+
 
 //a = asset tag number. If not set, let the user search for one
 if(ISSET($_GET['a'])){
-?>
-<html>
-	<!-- Initalize Page -->
-	<head>
-		<?php echo '<title>SHU DoIT | Asset# ', $_GET['a'] ,'</title>'; ?>
-		<link rel="stylesheet" type="text/css" href="style.css">
-	</head>
 	
-	<body>
-		<div id="main">
-			<?php include('header.html') ?>
-			<br><br></br>
-			<?php include('asset-display-form.php'); ?>
-		</div>
-	</body>
-</html>
-<?php
-//Need to connect to mysql. Search DB like SELECT * FROM assets WHERE asset like %$_GET['a']%
+	$asse = $_GET['a'];
+	$search_query = mysqli_query($con, "SELECT * FROM assets WHERE asset LIKE '%$asse%' ORDER BY asset ASC LIMIT 25");
+	$search_nums = mysqli_num_rows($search_query);
+	
+	if($search_nums == 1){
+		?>
+		<html>
+			<!-- Initalize Page -->
+			<head>
+				<?php echo '<title>SHU DoIT | Asset# ', $_GET['a'] ,'</title>'; ?>
+				<link rel="stylesheet" type="text/css" href="style.css">
+			</head>
+	
+			<body>
+				<div id="main">
+					<?php include('header.html') ?>
+					<br><br></br>
+					<?php include('asset-display-form.php'); ?>
+				</div>
+			</body>
+		</html>
+		<?php
+	}
+	elseif($search_nums > 1){
+		?>
+		<html>
+			<!-- Initalize Page -->
+			<head>
+				<?php echo '<title>SHU DoIT | Search  #', $_GET['a'] ,'</title>'; ?>
+				<link rel="stylesheet" type="text/css" href="style.css">
+			</head>
+	
+			<body>
+				<div id="main">
+					<?php include('header.html') ?>
+					<br><br></br>
+					<?php include('search-display.php'); ?>
+				</div>
+			</body>
+		</html>
+		<?php
+	}
+	elseif($search_nums == 0){
+		?>
+		<html>
+			<!-- Initalize Page -->
+			<head>
+				<?php echo '<title>SHU DoIT | Search  #', $_GET['a'] ,'</title>'; ?>
+				<link rel="stylesheet" type="text/css" href="style.css">
+			</head>
+	
+			<body>
+				<div id="main">
+					<?php include('header.html') ?>
+					<br><br></br>
+					<?php include('asset-display-form.php'); ?>
+				</div>
+			</body>
+		</html>
+		<?php
+	}
+
 }
 //Handle for asset name search
 elseif(ISSET($_GET['n'])){
