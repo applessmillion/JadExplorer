8e2f74923876b0dbaaaa43f73e02e8bb32e0726f
applessmillion <appless9009@gmail.com>
2018-11-04 02:03:06 -0500

    misc changes

--- web/config.php
+++ web/config.php
@@ -1,7 +1,6 @@
 <?php
 // For testing purposes, this user has FULL db permissions
-// Database server shared with Maralook.com
-    $ip   = "localhost"; //MySQL Server IP (l27.0.0.1)
+    $ip   = "localhost";
     $user = "applessmillion"; //User for db
     $pw   = "appless9009"; //User's password
     $db   = "shuit"; //Database to connect to
