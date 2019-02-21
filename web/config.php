dd8c77a934d8b343eeb3419989a333c7082c3a2f
Ben Robert <me@jadefury.com>
2019-02-21 08:58:41 -0500

    gitignore didn't work

--- web/config.php
+++ web/config.php
@@ -2,8 +2,8 @@
 /* This page is used as the holder of mySQL connection info.
 It is simply here now to show the variables and such.
 */
-$ip   = "107.180.51.34"; //MySQL Server IP
-$user = "shu_navigator"; //MySQL user
-$pw   = "jadedfury"; //MySQL password
-$db   = "shuexplorer"; //Database
+$ip   = ""; //MySQL Server IP
+$user = ""; //MySQL user
+$pw   = ""; //MySQL password
+$db   = ""; //Database
 ?>
