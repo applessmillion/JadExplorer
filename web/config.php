6f86977b1cfb77bc5483946ee4e149d6507a0c7c
applessmillion <appless9009@gmail.com>
2019-02-22 17:47:25 -0500

    Ready for multiple accounts

--- web/config.php
+++ web/config.php
@@ -3,7 +3,20 @@
 It is simply here now to show the variables and such.
 */
 $ip   = ""; //MySQL Server IP
+$db   = ""; //Database
+
+### Standard account. Should have READ & basic access only.
+# This account will fetch info from our db.
 $user = ""; //MySQL user
 $pw   = ""; //MySQL password
-$db   = ""; //Database
+
+### Privileged account. Should have WRITE access and a bit more.
+# This account is used in the creationtool.php. 
+# Eventually it will also need to edit records.
+$user = ""; //MySQL PRIVILEGED user
+$pw   = ""; //MySQL PRIVILEGED password
+
+
+
+
 ?>
