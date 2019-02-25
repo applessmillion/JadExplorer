d5d7dcea3f9bb6959622acc49c321036a7df044a
Ben Robert <me@jadefury.com>
2019-02-25 15:14:04 -0500

    Updating scripts

--- scripts/UpdateAsset.ps1
+++ scripts/UpdateAsset.ps1
@@ -2,8 +2,9 @@
 # Updates and adds info for an existing asset.
 
 ### Gather method info.
-$computer_name = Get-ChildItem Env:Computername;
-$computer_servicetag = wmic bios get serialnumber;
+$computer_name = Get-CimInstance -ClassName Win32_ComputerSystem -Property Name | Select-Object -ExpandProperty Name;
+$computer_servicetag = Get-CIMInstance -ClassName win32_bios -Property SerialNumber | Select-Object -ExpandProperty SerialNumber;
+$computer_ip = Get-NetIPAddress -PrefixOrigin Dhcp | Select-Object -ExpandProperty IPAddress;
 
 ### Check if servicecode is defined. If not, we gotta update via tag number.
 if(!$computer_servicetag){ $method = "tag" }
@@ -13,7 +14,7 @@ else{ $method = "service" }
 $curuser = Get-ChildItem Env:Username # Requires a .Value to pull value.
 
 ### URL to visit. As long as the above variables do not contain an &, we should be fine.
-$visit_url = "http://www.junklands.com/web/updatetool.php?method="+$method
+$visit_url = "http://www.junklands.com/web/updatetool.php?method="+$method+"&cname="+$computer_name+"&ctag="$computer_servicetag+"&cip="+$computer_ip;
 
 ### Run the command to load the URL.
 Invoke-WebRequest -Uri $visit_url;
