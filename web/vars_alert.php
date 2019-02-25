9791e534396d2d2ccd95cf5d10f3ea17a25acbde
Ben Robert <me@jadefury.com>
2019-02-25 15:13:43 -0500

    Removing specific alert

--- web/vars_alert.php
+++ web/vars_alert.php
@@ -5,11 +5,11 @@
 ### Alert text variables. 
 # Just define a value for alert_text and the alert will display.
 	$alert_title = "NOTICE";
-	$alert_text = "mySQL user has had most of it's access revoked. Searching, along with some other things, may be broken.";
+	$alert_text = "";
 
 ## Alert box customization. Colors yay!
-	$webpage_alert_border_color = "#7F0000";							//Alert border color. Prefer a darker color than the bg
-	$webpage_alert_bg_color = "#FF5E5E";								//Alert bg color. Should be ligher color, cuz text is black.
+	$webpage_alert_border_color = "#7F0000";	//Alert border color. Prefer a darker color than the bg
+	$webpage_alert_bg_color = "#FF5E5E";		//Alert bg color. Should be ligher color, cuz text is black.
 
 ## Other variables are located in the vars.php file.
 ?>
