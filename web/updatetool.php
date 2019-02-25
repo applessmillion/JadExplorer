5303997401168b62802c54856eb14ba9a4ee93e9
Ben Robert <me@jadefury.com>
2019-02-25 16:04:06 -0500

    Update updatetool.php

--- web/updatetool.php
+++ web/updatetool.php
@@ -6,20 +6,25 @@
 ### Check to see if the 	
 if(isset($_GET['method'])){
 	
-	$search = $_GET['method'];
+	$searchmethod = $_GET['method'];
+	$assetserial	= $_GET['cserial'];
+	$assettag 		= $_GET['cname'];
+		$assettag = substr($assettag, -5, strpos($assettag, '-')); //Grabs the last 5 chars in the string. Hope people aren't dumb and input the name correctly.
+	$assetIP = $_GET['cip'];
+	$assetuser = $_GET['cuser'];
 	
 	### For updates, we should expect data to change a lot. This means we need to have multiple methods to find the device.
 	
 	### Method #1 - Service Tag. These are fairly unique, and if it has one, that's great. They never change.
 	# Prefer this method as human error is next to none since it's added automatically.
-		if($_GET['method'] == "service"){
-			$sql_checkfordevice = "SELECT * FROM asset_information WHERE serviceno='$search' ORDER BY creation_date LIMIT 1";
+		if($searchmethod == "service"){
+			$sql_checkfordevice = "SELECT * FROM asset_information WHERE serviceno='$assetserial' ORDER BY creation_date LIMIT 1";
 			echo "Submitting update information based on <B>SERVICE TAG</B></br>";
 		}
 	### Method #2 - Asset tag. These should never change, but devices may change.
 	# In case we run into multiple entries, the SQL statement will fetch the most recently added asset.
-		elseif($_GET['method'] == "tag"){
-			$sql_checkfordevice = "SELECT * FROM asset_information WHERE tagno='$search' ORDER BY creation_date LIMIT 1";
+		elseif($searchmethod == "tag"){
+			$sql_checkfordevice = "SELECT * FROM asset_information WHERE tagno='$assettag' ORDER BY creation_date LIMIT 1";
 			echo "Submitting update information based on <B>ASSET NUMBER</B></br>";
 		}
 	### Method #3 - Undefined
