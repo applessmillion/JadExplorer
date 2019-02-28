2bcfa055707f35fea331de823ca745b07140d2d3
Ben Robert <me@jadefury.com>
2019-02-28 15:37:44 -0500

    News can now be toggled off.

--- web/vars_news.php
+++ web/vars_news.php
@@ -2,23 +2,29 @@
 #########################
 #    RECENT NEWS VARS   #
 #########################
-### Recent news variables.
+$DISPLAY_NEWS = true;
 
 ### News and stuff. Displays in the $widget_updates.
 	$text_recentnews_title = "Recent Updates & Notices";
-	$text_recentnews_first_date = "February 26, 2018";
-	$text_recentnews_first_text = "More info is now displayed on asset pages.";
+	$text_recentnews_first_date = "February 28, 2018";
+	$text_recentnews_first_text = "Device Service Tags for Dell devices will now link to Dell's website with info on the product.";
 	$text_recentnews_second_date = "February 26, 2018";
 	$text_recentnews_second_text = "Working on optimizing page backgrounds and load times.";
 	
 	
 ### Recent News Widget
-	$widget_updates = "
-		<h3>$text_recentnews_title</h3>
-		<p class='mx-5 text-left'><strong>$text_recentnews_first_date</strong> - $text_recentnews_first_text</a></br>
-		<strong>$text_recentnews_second_date</strong> - $text_recentnews_second_text</p>
-		</br>
-	";
+# Let's give myself an option to toggle visibility.
+	if($DISPLAY_NEWS == false){
+		$widget_updates = "";
+	}
+	else{
+		$widget_updates = "
+			<h3>$text_recentnews_title</h3>
+			<p class='mx-5 text-left'><strong>$text_recentnews_first_date</strong> - $text_recentnews_first_text</a></br>
+			<strong>$text_recentnews_second_date</strong> - $text_recentnews_second_text</p>
+			</br>
+		";
+	}
 
 ## Other variables are located in the vars.php file.
 ?>
