346bb5802924cbf709108a5e5e1a6abf7225fbaa
Ben Robert <me@jadefury.com>
2019-03-11 08:49:30 -0400

    I dislike DST

--- web/vars_alert.php
+++ web/vars_alert.php
@@ -4,8 +4,8 @@
 #########################
 ### Alert text variables. 
 # Just define a value for alert_text and the alert will display.
-	$alert_title = "";
-	$alert_text = "";
+	$alert_title = "Daylight Savings";
+	$alert_text = "DST is in effect, meaning times after March 10 will be an hour behind. This is due to the server clock not changing.";
 
 ## Alert box customization. Colors yay!
 	$webpage_alert_border_color = "#7F0000";	//Alert border color. Prefer a darker color than the bg
@@ -15,7 +15,7 @@
 	$widget_webpage_alert = '
 		<div class="card" style="background-color:'.$webpage_alert_bg_color.';border:2px solid '.$webpage_alert_border_color.'">
 			<b class="mt-1 text-center" style="font-size:24px;">'. $alert_title .'</b>
-			<b class="my-1 text-left" "style="font-size:16px;">'.$alert_text.'</b>
+			<b class="mx-2 my-1 text-left" "style="font-size:16px;">'.$alert_text.'</b>
 		</div>
 		</br></br>';
 		
