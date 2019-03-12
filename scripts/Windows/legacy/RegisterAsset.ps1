c5f0e1c2ba0547985d2098e3a491f802606df256
Ben Robert <me@jadefury.com>
2019-03-12 15:04:40 -0400

    Moving to legacy
    Will remove these scripts once the POST varients are complete.

--- scripts/Windows/legacy/RegisterAsset.ps1
+++ scripts/Windows/legacy/RegisterAsset.ps1
@@ -2,11 +2,8 @@
 # For use during the Windows imaging process.
 # Can also be run on already imaged machines to add the device to SHU-Explorer db.
 
-### Set some basic settings, such as the URL we're visiting and powershell-specific settings.
-$ProgressPreference = 'SilentlyContinue';
-$visit_url = "http://www.junklands.com/web/e/createasset.php";
-
 ### Gather computer information. Gathers the most basic information.
+# Formatting is not a concern, as our php page will neaten up everything before entered to the db.
 $computer_name = Get-CimInstance -ClassName Win32_ComputerSystem -Property Name | Select-Object -ExpandProperty Name;
 $computer_model = Get-CimInstance -ClassName Win32_ComputerSystem -Property Model | Select-Object -ExpandProperty Model;
 $computer_manufacturer = Get-CimInstance -ClassName Win32_ComputerSystem -Property Manufacturer | Select-Object -ExpandProperty Manufacturer;
@@ -14,17 +11,11 @@ $computer_servicetag = Get-CIMInstance -ClassName win32_bios -Property SerialNum
 $computer_winserial = Get-CIMInstance -ClassName Win32_OperatingSystem -Property SerialNumber | Select-Object -ExpandProperty SerialNumber;
 $computer_ethernet = Get-NetAdapter -Name Ethernet | Select-Object -ExpandProperty MacAddress;
 
-### Allows for some basic security. PHP can check to make sure this var is the same on it's side.
-$verify = 1;
+### URL to visit. As long as the above variables do not contain an &, we should be fine.
+$visit_url = "http://www.junklands.com/web/creationtool.php?cname="+$computer_name+"&cmodel="+$computer_model+"&cserial="+$computer_winserial+"&cmanu="+$computer_manufacturer+"&cservice="+$computer_servicetag+"&cat=1&ethernet="+$computer_ethernet;
 
-### Compile all the above info.
-$submit_variables = @{cname=$computer_name;cmodel=$computer_model;cserial=$computer_winserial;
- cmanu=$computer_manufacturer;cservice=$computer_servicetag;ethernet=$computer_ethernet;
- "cat"=1;v=$verify}
-					
-### Visit page and submit the POST variables.
-Invoke-WebRequest -Uri $visit_url -Method POST -Body $submit_variables
+### Do the web request
+Invoke-WebRequest -Uri $visit_url
 
-### Close out of this script.
 exit
 
