6a332ae42fd05ff1725d40e9339294813fc4e849
Ben Robert <me@jadefury.com>
2019-03-18 10:59:19 -0400

    Update copyright_notice

--- web/vars/contact.php
+++ web/vars/contact.php
@@ -6,7 +6,7 @@
 	$contact_email = "contact@jadefury.com";
 	$link_github = "https://github.com/applessmillion/";
 ### Copyright notice for SHU-Explorer.
-	$copyright_notice = "Copyright 2019. JADEFURY LLC/Benjamin Robert. All Rights Reserved.";
+	$copyright_notice = "Copyright 2019 JADEFURY LLC.";
 
 ## Other variables are located in the vars.php file.
 ?>
